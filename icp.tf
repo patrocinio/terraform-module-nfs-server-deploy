41278c5e344bf6742a8608c12020b3af1687aac9
Eduardo Patrocinio <epatro@gmail.com>
2017-10-05 14:47:18 -0400

    Remove icp cluster

--- icp.tf
+++ icp.tf
@@ -9,53 +9,6 @@ resource "tls_private_key" "icpkey" {
   }
 }
 
-## Actions that has to be taken on all nodes in the cluster
-resource "null_resource" "icp-cluster" {
-
-  count = "${var.cluster_size}"
-  
-  connection {
-      host = "${element(local.icp-ips, count.index)}"
-      user = "${var.ssh_user}"
-      private_key = "${file(var.ssh_key)}"
-  }
-   
-  # Validate we can do passwordless sudo in case we are not root
-  provisioner "remote-exec" {
-    inline = [
-      "sudo -n echo This will fail unless we have passwordless sudo access"
-    ]
-  }
-
-  provisioner "file" {
-      content = "${var.generate_key ? tls_private_key.icpkey.public_key_openssh : file(var.icp_pub_keyfile)}"
-      destination = "/tmp/icpkey"
-  
-  }
-  
-  provisioner "remote-exec" {
-    inline = [
-      "mkdir -p /tmp/icp-common-scripts"
-    ]
-  }
-  provisioner "file" {
-    source      = "${path.module}/scripts/common/"
-    destination = "/tmp/icp-common-scripts"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "mkdir -p ~/.ssh",
-      "cat /tmp/icpkey >> ~/.ssh/authorized_keys",
-      "chmod a+x /tmp/icp-common-scripts/*",
-      "/tmp/icp-common-scripts/prereqs.sh",
-      "/tmp/icp-common-scripts/version-specific.sh ${var.icp-version}",
-      "/tmp/icp-common-scripts/docker-user.sh"
-    ]
-  }
-}
-
-
 ## Actions that needs to be taken on boot master only
 resource "null_resource" "icp-boot" {
 
